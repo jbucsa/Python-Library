a4ce4716a98a57a7413876ff0bdf7df670616779
jbucsa <justin.bucsa@gmail.com>
2024-02-22 21:08:25 -0600

    .env routing added to the __init__.py

--- Flask-Projects/Pet-Fax/petfax/__init__.py
+++ Flask-Projects/Pet-Fax/petfax/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_migrate import Migrate 
 
-import dotenv
+from dotenv import load_dotenv
+import os
 
-dotenv.load_dotenv()
+load_dotenv()
 
 # Factory 
           
@@ -11,7 +12,7 @@ def create_app():
     app = Flask(__name__)
            
     # database config 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from . import models 
