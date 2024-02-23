d5467d8a5337b14718cf7ee11ee74357ad46911e
jbucsa <justin.bucsa@gmail.com>
2024-02-22 21:35:36 -0600

    Safe point to save for Pet-Fax project.

--- Flask-Projects/Pet-Fax/petfax/__init__.py
+++ Flask-Projects/Pet-Fax/petfax/__init__.py
@@ -28,4 +28,8 @@ def create_app():
     from . import pet
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint 
+    # from . import fact
+    # app.register_blueprint(fact.bp)
+
     return app
--- Flask-Projects/Pet-Fax/petfax/fact.py
+++ Flask-Projects/Pet-Fax/petfax/fact.py
@@ -1,4 +1,5 @@
 from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
@@ -7,10 +8,21 @@ def index():
     print(request.form)
     return 'Thanks for submitting a fun fact!'
 
-@bp.route('/', methods=['GET', 'POST']) 
+
+@bp.route('/', methods=['GET', 'POST'])
 def index(): 
     if request.method == 'POST':
-       print(request.form)
-       return redirect('/facts')
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+    results = models.Fact.query.all()
     
-    return render_template('facts/index.html')
+    return render_template('facts/index.html', facts=results)
+
+@bp.route('/new')
+def new(): 
+    return render_template('facts/new.html')
